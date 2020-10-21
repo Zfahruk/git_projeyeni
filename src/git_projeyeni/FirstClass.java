a1728fbe04ae094d98233fa85b218fd4b8645c4a
Zfahruk <synergyetkisi@gmail.com>
2020-10-21 20:01:10 +0200

    ikinci commit localden

--- src/git_projeyeni/FirstClass.java
+++ src/git_projeyeni/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("ilk commit saat 19:47");
+		System.out.println("ilk commit local den saat 19:58");
 
 	}
 
